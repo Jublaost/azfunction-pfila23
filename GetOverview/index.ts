759db98e40013f940efbc97aa3676015e2ee9704
Thomas <65050898+tvonment@users.noreply.github.com>
2022-10-27 09:19:39 +0000

    add overview

---
+++ GetOverview/index.ts
@@ -0,0 +1,28 @@
+import { AzureFunction, Context, HttpRequest } from "@azure/functions"
+
+const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest, scharenIn): Promise<void> {
+    context.log('HTTP trigger function processed a request.');
+
+    let message = "";
+    message += `Total Scharen: ${scharenIn.length}\n`
+
+    let count_tn = 0;
+    let count_leader = 0;
+
+    for (let schar of scharenIn) {
+        count_tn += parseInt(schar.num_tn);
+        count_leader += parseInt(schar.num_leader);
+    }
+
+    message += `Total TN: ${count_tn}\n`
+    message += `Total Leiter: ${count_leader}\n`
+    message += `Total: ${count_tn + count_leader}\n`
+
+    context.res = {
+        // status: 200, /* Defaults to 200 */
+        body: message
+    };
+
+};
+
+export default httpTrigger;
