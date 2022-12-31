c8dcd5635892d17c492ed6aede49cc6d4ccbd24f
thomas <thvome@gmail.com>
2022-12-31 11:50:27 +0100

    add scharkontakte

---
+++ GetScharKontakte/index.ts
@@ -0,0 +1,21 @@
+import { AzureFunction, Context, HttpRequest } from "@azure/functions"
+
+const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest, scharenIn): Promise<void> {
+    context.log('HTTP trigger function processed a request.');
+
+    let message = "";
+
+    for (let schar of scharenIn) {
+        message += schar.schar + '\n';
+        message += schar.kontakt + '\n';
+        message += schar.email + '\n\n';
+    }
+
+    context.res = {
+        // status: 200, /* Defaults to 200 */
+        body: message
+    };
+
+};
+
+export default httpTrigger;
